67a44d75dae5528dc0317d2e1b8595e5b2093263
MarkoGlamocak <glamocakmarko99@gmail.com>
2021-11-11 19:17:11 -0600

    Basic Table of Top Ten Trending

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,8 +1,22 @@
 fetch('http://localhost:3000/user')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        console.log(data);
         document.getElementById('name').innerHTML = 'Welcome, ' + data.first_name;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+fetch('http://localhost:3000/movie/trending')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let tableRows = '';
+        for (let i = 0; i < 10; i++) {
+            tableRows = tableRows + '<tr> <td>' + (i+1) + '</td> <td>' + data[i].title + '</td> <td>' + data[i].vote_average + '</td> <td>' + data[i].vote_count + '</td> </tr>';
+        }
+        document.getElementById('topTrendingTable').innerHTML = tableRows;
     })
     .catch((error) => {
         console.error('Error:', error);
