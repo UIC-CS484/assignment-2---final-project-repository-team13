6a3b3210a76a1844f9365237b58d55d13356bdc0
MarkoGlamocak <glamocakmarko99@gmail.com>
2021-12-01 02:30:04 -0600

    Added your watched movies section and your liked movies section to account homepage

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,3 +1,8 @@
+let watchedMovies;
+let likedMovies;
+let watchedPage = 1;
+let likedPage = 1;
+
 fetch('http://localhost:3000/user')
     .then(response => response.json())
     .then(data => {
@@ -6,7 +11,117 @@ fetch('http://localhost:3000/user')
     })
     .catch((error) => {
         console.error('Error:', error);
-    });
+});
+
+fetch('http://localhost:3000/api/watch')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        watchedMovies = data;
+        let movieItems = "";
+        let index = 0;
+        let count = 0;
+        while (index < watchedMovies.length && count != 5) {
+            let imagelink = 'https://image.tmdb.org/t/p/w500' + watchedMovies[index].image;
+            movieItems = movieItems + "<div class='movieItem'><img class='moviePoster' src="+imagelink+" alt='MoviePoster'><p>"+watchedMovies[index].name+"</p></div>";
+            index++;
+            count++;
+        }
+        document.getElementById('watchedMovies').innerHTML = movieItems;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+});
+
+fetch('http://localhost:3000/api/like')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        likedMovies = data;
+        let movieItems = "";
+        let index = 0;
+        let count = 0;
+        while (index < likedMovies.length && count != 5) {
+            let imagelink = 'https://image.tmdb.org/t/p/w500' + likedMovies[index].image;
+            movieItems = movieItems + "<div class='movieItem'><img class='moviePoster' src="+imagelink+" alt='MoviePoster'><p>"+likedMovies[index].name+"</p></div>";
+            index++;
+            count++;
+        }
+        document.getElementById('likedMovies').innerHTML = movieItems;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+});
+
+function moveToPrevPage(type) {
+    if (type == 'watched') {
+        document.getElementById('moveNextWatched').innerHTML = `<img src="/resources/Next.png" alt="Next">`;
+        if (watchedPage == 1) {
+            document.getElementById('movePrevWatched').innerHTML = "";
+        } else {
+            watchedPage = watchedPage - 1;
+            if (watchedPage == 1) {
+                document.getElementById('movePrevWatched').innerHTML = "";
+            }
+            loadPage('watched');
+        }
+    } else if (type == 'liked') {
+        document.getElementById('moveNextLiked').innerHTML = `<img src="/resources/Next.png" alt="Next">`;
+        if (likedPage == 1) {
+            document.getElementById('movePrevLiked').innerHTML = "";
+        } else {
+            likedPage = likedPage - 1;
+            if (likedPage == 1) {
+                document.getElementById('movePrevLiked').innerHTML = "";
+            }
+            loadPage('liked');
+        }
+    }
+}
+
+function moveToNextPage(type) {
+    if (type == 'watched') {
+        document.getElementById('movePrevWatched').innerHTML = `<img src="/resources/Previous.png" alt="Next">`;
+        watchedPage = watchedPage + 1;
+        if (watchedPage * 5 > watchedMovies.length - 1) {
+            document.getElementById('moveNextWatched').innerHTML = '';
+        }
+        loadPage('watched');
+    } else if (type == 'liked') {
+        document.getElementById('movePrevLiked').innerHTML = `<img src="/resources/Previous.png" alt="Next">`;
+        likedPage = likedPage + 1;
+        if (likedPage * 5 > likedMovies.length - 1) {
+            document.getElementById('moveNextLiked').innerHTML = '';
+        }
+        loadPage('liked');
+    }
+}
+
+function loadPage(type) {
+    if (type == 'watched') {
+        let movieItems = "";
+        let index = (watchedPage - 1) * 5;
+        let count = 0;
+        while (index < watchedMovies.length && count != 5) {
+            let imagelink = 'https://image.tmdb.org/t/p/w500' + watchedMovies[index].image;
+            movieItems = movieItems + "<div class='movieItem'><img class='moviePoster' src="+imagelink+" alt='MoviePoster'><p>"+watchedMovies[index].name+"</p></div>";
+            index++;
+            count++;
+        }
+        document.getElementById('watchedMovies').innerHTML = movieItems;
+    } else if (type == 'liked') {
+        let movieItems = "";
+        let index = (likedPage - 1) * 5;
+        let count = 0;
+        while (index < likedMovies.length && count != 5) {
+            let imagelink = 'https://image.tmdb.org/t/p/w500' + likedMovies[index].image;
+            movieItems = movieItems + "<div class='movieItem'><img class='moviePoster' src="+imagelink+" alt='MoviePoster'><p>"+likedMovies[index].name+"</p></div>";
+            index++;
+            count++;
+        }
+        document.getElementById('likedMovies').innerHTML = movieItems;
+    }
+}
 
 function goBack() {
     location.href = '/account';
