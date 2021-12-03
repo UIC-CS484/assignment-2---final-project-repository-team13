99e39a6b8bc60d70306d04ac60dfa76c0d8f62b7
MarkoGlamocak <glamocakmarko99@gmail.com>
2021-12-03 16:14:23 -0600

    Fixed Password Enforcement in Front End

--- public/javascripts/createAccount.js
+++ public/javascripts/createAccount.js
@@ -21,7 +21,15 @@ document.querySelector('#btn').addEventListener('click', (e) => {
             .then(response => response.json())
             .then(data => {
                 console.log('Success:', data);
-                if (data.success == false) {
+                if (data.success == false && data.error == 'password is not strong enough') {
+                    document.getElementById('errorMessage').innerHTML = 'Password Must Be At Least 8 Characters!';
+                } else if (data.success == false && data.error == 'password is easy to guess') {
+                    document.getElementById('errorMessage').innerHTML = 'Password Cannot Match Email!';
+                } else if (data.success == false && data.error == 'restricted password') {
+                    document.getElementById('errorMessage').innerHTML = 'This Password is Restricted!';
+                } else if (data.success == false && data.error == 'password need to have alphabet letters') {
+                    document.getElementById('errorMessage').innerHTML = 'Password Must Include Character!';
+                } else if (data.success == false) {
                     document.getElementById('errorMessage').innerHTML = 'Account Already Exists!';
                 } else {
                     location.href = '/login'
--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -33,7 +33,22 @@ document.querySelector('#passBtn').addEventListener('click', (e) => {
         .then(response => response.json())
         .then(data => {
             console.log('Success:', data);
-            document.getElementById('message').innerHTML = 'Password has changed!';
+            if (data.success == false && data.error == 'password is not strong enough') {
+                document.getElementById('errorMessage').innerHTML = 'Password Must Be At Least 8 Characters!';
+                document.getElementById('message').innerHTML = '';
+            } else if (data.success == false && data.error == 'password is easy to guess') {
+                document.getElementById('errorMessage').innerHTML = 'Password Cannot Match Email!';
+                document.getElementById('message').innerHTML = '';
+            } else if (data.success == false && data.error == 'restricted password') {
+                document.getElementById('errorMessage').innerHTML = 'This Password is Restricted!';
+                document.getElementById('message').innerHTML = '';
+            } else if (data.success == false && data.error == 'password need to have alphabet letters') {
+                document.getElementById('errorMessage').innerHTML = 'Password Must Include Character!';
+                document.getElementById('message').innerHTML = '';
+            } else if (data.success == true) {
+                document.getElementById('message').innerHTML = 'Password has changed!';
+                document.getElementById('errorMessage').innerHTML = '';
+            }
         })
         .catch((error) => {
             console.error('Error:', error);
